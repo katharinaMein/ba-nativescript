07f89889ee30b9babb5024d20f4566effb846366
DESKTOP-EJO1RF7\katha <katharina@rseibert.de>
2022-08-27 13:09:37 +0200

    wip

--- src/app/microphone/microphone.component.ts
+++ src/app/microphone/microphone.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AudioRecorderOptions, TNSPlayer} from 'nativescript-audio';
+import {TNSRecorder} from 'nativescript-audio';
+import {knownFolders} from '@nativescript/core/file-system';
+import {catchError, EMPTY, from, Observable, of} from "rxjs";
 
 @Component({
   selector: 'app-microphone',
@@ -6,9 +10,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./microphone.component.css']
 })
 export class MicrophoneComponent {
+  player: TNSPlayer;
+  recorder: TNSRecorder;
+  recording = false;
+  audioRecorded = false;
 
-  constructor() {}
+  audioFolder = knownFolders.currentApp().getFolder('recordings');
+
+  options: AudioRecorderOptions = {
+    filename: this.audioFolder.path + '/recording.mp4',
+    infoCallback: info => {
+      //apparently necessary even if blank
+    },
+    errorCallback: e => {
+      console.log('error cb', e);
+    }
+  };
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  onClickRecordButton() {
+    if (!this.recorder) {
+      this.onCreateRecorder();
+    }
+    this.onToggleRecording(this.recording);
+  }
+
+  onToggleRecording(recording: boolean) {
+    recording ? this.recorder.stop() : this.recorder.start(this.options);
+    this.recording = !this.recording;
+    if (!this.recording) {
+      this.audioRecorded = true;
+    }
+  }
+
+  onCreateRecorder() {
+    this.recorder = new TNSRecorder();
+  }
+
+  onPlay() {
+    this.player = new TNSPlayer();
+    from(this.player.playFromFile({
+        loop: false,
+        audioFile: this.audioFolder.path + '/recording.mp4'
+      }
+    )).pipe(
+      catchError(err => {
+        alert(err);
+        return EMPTY;
+      })
+    ).subscribe();
   }
 }
--- src/app/wake-lock/wake-lock.component.ts
+++ src/app/wake-lock/wake-lock.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { keepAwake, allowSleepAgain } from "nativescript-insomnia";
 
 @Component({
   selector: 'app-wake-lock',
   templateUrl: './wake-lock.component.html',
   styleUrls: ['./wake-lock.component.css']
 })
-export class WakeLockComponent {
+export class WakeLockComponent implements OnInit{
+  insomnia = false;
 
   constructor() {}
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.onDeactivateInsomnia();
+  }
+
+  onToggleWakeLock(){
+    if(!this.insomnia){
+      this.onActivateInsomnia();
+    }else{
+      this.onDeactivateInsomnia();
+    }
+  }
+
+  onActivateInsomnia(){
+    keepAwake().then(function() {
+      this.insomnia = true;
+    }.bind(this))
+  }
+
+  onDeactivateInsomnia(){
+    allowSleepAgain().then(function() {
+      this.insomnia = false;
+    }.bind(this))
   }
 }
